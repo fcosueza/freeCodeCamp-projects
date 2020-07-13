f0268610a6c8635ecb2f88fedb1900a4e3b65ae1
Francisco Sueza <fcosueza@disroot.org>
2020-07-13 20:31:20 +0200

    Added palindrome solution

--- javascript-algorithms/palindrome/palindrome.js
+++ javascript-algorithms/palindrome/palindrome.js
@@ -1,3 +1,10 @@
-const palindrome = str => str === "";
+const palindrome = str => {
+  const formatString = str
+    .match(/[^\W_]/g)
+    .join("")
+    .toLowerCase();
+
+  return formatString === formatString.split("").reverse().join("");
+};
 
 export default palindrome;
