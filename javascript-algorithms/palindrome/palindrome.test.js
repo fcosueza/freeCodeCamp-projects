4edfa82fa31d8c508383c88895992a7acc5b4ad6
Francisco Sueza <fcosueza@disroot.org>
2020-07-13 18:26:44 +0200

    Added more tests

--- javascript-algorithms/palindrome/palindrome.test.js
+++ javascript-algorithms/palindrome/palindrome.test.js
@@ -1,7 +1,31 @@
 import palindrome from "./palindrome";
 
-describe("palindrome() check if a string is a palindrome", () => {
-  test("should return boolean", () => {
+describe("Check if a string is a palindrome, deleting non-alphanumerical characters", () => {
+  test("return a boolean", () => {
     expect(typeof palindrome("eye")).toBe("boolean");
+  });
+
+  test("check a palindrome with only letters", () => {
+    expect(palindrome("eye")).toBe(true);
+  });
+
+  test("check a palindrome with non-alphanumeric characters", () => {
+    expect(palindrome("_eye")).toBe(true);
+  });
+
+  test("Check a palindrome with spaces", () => {
+    expect(palindrome("race car")).toBe(true);
+  });
+
+  test("Check not a palindrome with only letters", () => {
+    expect(palindrome("nope")).toBe(false);
+  });
+
+  test("Check not a palindrome with non-alphanumeric characters", () => {
+    expect(palindrome("five|_/|four")).toBe(false);
+  });
+
+  test("Check not a palindrome with spaces", () => {
+    expect(palindrome("not a palindrome")).toBe(false);
   });
 });
