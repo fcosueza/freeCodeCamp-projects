65da277398fd73b534be5b511c58fc6cc8a73f82
Francisco Sueza <fcosueza@disroot.org>
2020-07-15 18:34:24 +0200

    Changed some palindrome tests descriptions

--- javascript-algorithms/palindrome/palindrome.test.js
+++ javascript-algorithms/palindrome/palindrome.test.js
@@ -1,47 +1,47 @@
 import palindrome from "./palindrome";
 
-describe("Check if a string is a palindrome ignoring non-alphanumerical characters", () => {
+describe("Check if a string is a palindrome", () => {
   test("return a boolean", () => {
     expect(typeof palindrome("eye")).toBe("boolean");
   });
 
-  test("check a palindrome with only letters", () => {
+  test("Palindrome with only letters", () => {
     expect(palindrome("eye")).toBe(true);
   });
 
-  test("check a palindrome with non-alphanumeric characters", () => {
+  test("Palindrome with non-alphanumeric characters", () => {
     expect(palindrome("_eye")).toBe(true);
   });
 
-  test("Check a palindrome with spaces", () => {
+  test("Palindrome with spaces", () => {
     expect(palindrome("race car")).toBe(true);
   });
 
-  test("Check a palindrome with spaces and non-alphanumerical characters", () => {
+  test("Palindrome with spaces and non-alphanumerical chars", () => {
     expect(palindrome("My age is 0, 0 si ega ym")).toBe(true);
   });
 
-  test("Check a palindrome with only numbers and non-alphanumerical characters", () => {
+  test("Palindrome with only numbers and non-alphanumerical chars", () => {
     expect(palindrome("0_0 (: /) 0_0")).toBe(true);
   });
 
-  test("Check not a palindrome with only letters", () => {
+  test("Not a palindrome with only letters", () => {
     expect(palindrome("nope")).toBe(false);
   });
 
-  test("Check not a palindrome with non-alphanumeric characters", () => {
+  test("Not a palindrome with non-alphanumeric characters", () => {
     expect(palindrome("five|_/|four")).toBe(false);
   });
 
-  test("Check not a palindrome with spaces", () => {
+  test("Not a palindrome with spaces", () => {
     expect(palindrome("not a palindrome")).toBe(false);
   });
 
-  test("Check not a palindrome with spaces and non-alphanumerical characters", () => {
+  test("Not a palindrome with spaces and non-alphanumerical chars", () => {
     expect(palindrome("My age is 0, 0 yes")).toBe(false);
   });
 
-  test("Check not a palindrome with only numbers and non-alphanumerical characters", () => {
+  test("Not a palindrome with numbers and non-alphanumerical chars", () => {
     expect(palindrome("0_1 (: /) 0_0")).toBe(false);
   });
 });
