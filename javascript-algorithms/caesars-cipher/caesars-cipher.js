23b120130e7f36a9ec9a588c23ae51f0497d85a7
Francisco Sueza <fcosueza@disroot.org>
2020-07-30 19:09:39 +0200

    Added caesars cipher solution

--- javascript-algorithms/caesars-cipher/caesars-cipher.js
+++ javascript-algorithms/caesars-cipher/caesars-cipher.js
@@ -1,3 +1,14 @@
-const rot13 = str => str;
+const rot13 = str => {
+  const regexp = /[A-Z]/g;
+  const lowerBound = 65;
+  const step = 13;
+  const width = 26;
+
+  return str.replace(regexp, c =>
+    String.fromCharCode(
+      ((c.charCodeAt(0) - lowerBound + step) % width) + lowerBound
+    )
+  );
+};
 
 export default rot13;
