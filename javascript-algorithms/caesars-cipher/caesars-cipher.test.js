592876d3aaa42862f8b3897dd2aad1cc78c9768a
Francisco Sueza <fcosueza@disroot.org>
2020-07-30 15:01:44 +0200

    Added caesars cipher tests

--- javascript-algorithms/caesars-cipher/caesars-cipher.test.js
+++ javascript-algorithms/caesars-cipher/caesars-cipher.test.js
@@ -1,1 +1,19 @@
 import rot13 from "./caesars-cipher";
+
+describe("Testing rot13 cipher algorithms function", () => {
+  test("Decode empty string", () => expect(rot13("")).toEqual(""));
+
+  test("Decode a string", () =>
+    expect(rot13("SERR PBQR PNZC")).toEqual("FREE CODE CAMP"));
+
+  test("Decode a string with an exclamation mark", () =>
+    expect(rot13("SERR CVMMN!")).toEqual("FREE PIZZA!"));
+
+  test("Decode a string with an interrogation mark", () =>
+    expect(rot13("SERR YBIR?")).toEqual("FREE LOVE?"));
+
+  test("Decode a long string", () =>
+    expect(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")).toEqual(
+      "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+    ));
+});
