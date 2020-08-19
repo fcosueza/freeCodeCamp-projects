d7fda997a2f072acda9e4de991e0f1c2ff32a3ce
Francisco Sueza <fcosueza@disroot.org>
2020-08-19 13:26:38 +0200

    Added more tests

--- javascript-algorithms/phone-validator/phone.validator.test.js
+++ javascript-algorithms/phone-validator/phone.validator.test.js
@@ -5,15 +5,39 @@ describe("Test telephoneCheck script against several phone formats", () => {
     expect(typeof telephoneCheck("555-555-5555")).toBe("boolean");
   });
 
-  test("Should return true with a standard phone number", () => {
+  test("Should return true with a valid number", () => {
     expect(telephoneCheck("555-555-5555")).toBe(true);
   });
 
-  test("Should return true with a standard number and a prefix", () => {
+  test("Should return true with a valid number with prefix", () => {
     expect(telephoneCheck("1 555-555-5555")).toBe(true);
   });
 
-  test("Should return true with a valid number without whitespaces", () => {
+  test("Should return true with a valid number without white spaces", () => {
     expect(telephoneCheck("5555555555")).toBe(true);
+  });
+
+  test("Should return true with a number with correct parenthesis", () => {
+    expect(telephoneCheck("(555) 555-5555")).toBe(true);
+  });
+
+  test("Should return true with a number with correct parenthesis and prefix", () => {
+    expect(telephoneCheck("1 (555) 555-5555")).toBe(true);
+  });
+
+  test("Should return false with invalid characters", () => {
+    expect(telephoneCheck("5as as?a 910n")).toBe(false);
+  });
+
+  test("Should return false with incorrect number of digits", () => {
+    expect(telephoneCheck("555-5555")).toBe(false);
+  });
+
+  test("Should return false with invalid prefix", () => {
+    expect(telephoneCheck("2 (555) 555-5555")).toBe(false);
+  });
+
+  test("Should return false with incorrect parenthesis", () => {
+    expect(telephoneCheck("(555 555-555")).toBe(false);
   });
 });
