9ea485a81a09e89b615f3929087c589973e0ba2a
Francisco Sueza <fcosueza@disroot.org>
2020-08-20 13:14:25 +0200

    A little refactor

--- javascript-algorithms/phone-validator/phone-validator.js
+++ javascript-algorithms/phone-validator/phone-validator.js
@@ -1,7 +1,5 @@
 const telephoneCheck = number => {
-  const regexp = new RegExp(
-    /^[1]{0,1}\s{0,1}(\([0-9]{3}\)|[0-9]{3})[-\s]{0,1}[0-9]{3}[-\s]{0,1}[0-9]{4}$/g
-  );
+  const regexp = /^[1]{0,1}\s{0,1}(\([0-9]{3}\)|[0-9]{3})[-\s]{0,1}[0-9]{3}[-\s]{0,1}[0-9]{4}$/g;
 
   return regexp.test(number);
 };
