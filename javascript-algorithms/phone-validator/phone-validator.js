7621e546791fbcc90a70ce45a95af6519a82090a
Francisco Sueza <fcosueza@disroot.org>
2020-08-20 12:34:42 +0200

    Added regexp

--- javascript-algorithms/phone-validator/phone-validator.js
+++ javascript-algorithms/phone-validator/phone-validator.js
@@ -1,3 +1,9 @@
-const telephoneCheck = number => !number;
+const telephoneCheck = number => {
+  const regexp = new RegExp(
+    /^[1]{0,1}\s{0,1}[(]{0,1}[0-9]{3}[)]{0,1}[-\s.0-9]{7,9}$/g
+  );
+
+  return regexp.test(number);
+};
 
 export default telephoneCheck;
