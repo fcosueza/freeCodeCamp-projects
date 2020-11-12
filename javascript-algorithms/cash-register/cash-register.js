da8861d8497b1e490eea68df77b795c868e9643c
Francisco Sueza <fcosueza@disroot.org>
2020-11-12 18:53:10 +0100

    Changed coinValues to Object from Array

--- javascript-algorithms/cash-register/cash-register.js
+++ javascript-algorithms/cash-register/cash-register.js
@@ -1,28 +1,28 @@
-const coinValues = [
-  ["PENNY", 0.01],
-  ["NICKEL", 0.05],
-  ["DIME", 0.1],
-  ["QUARTER", 0.25],
-  ["DOLLAR", 1],
-  ["FIVE", 5],
-  ["TEN", 10],
-  ["TWENTY", 20],
-  ["HUNDRED", 100]
-];
+const coinValues = {
+  PENNY: 0.01,
+  NICKEL: 0.05,
+  DIME: 0.1,
+  QUARTER: 0.25,
+  DOLLAR: 1,
+  FIVE: 5,
+  TEN: 10,
+  TWENTY: 20,
+  HUNDRED: 100
+};
 
 const checkCashRegister = (price, cash, cid) => {
   const availableChange = cid.reduce((acc, curr) => acc + curr[1], 0);
   const necessaryChange = cash - price;
-  const changeInDimes = {
-    status: "OPEN",
-    change: []
-  };
+  const changeInDimes = { status: "", change: [] };
 
   if (availableChange < necessaryChange) {
     changeInDimes.status = "INSUFFICIENT_FUNDS";
   } else if (availableChange === necessaryChange) {
     changeInDimes.status = "CLOSED";
     changeInDimes.change = cid;
+  } else {
+    changeInDimes.status = "OPEN";
+    changeInDimes.change = calculateCHange(necessaryChange, cid, coinValues);
   }
 
   return changeInDimes;
