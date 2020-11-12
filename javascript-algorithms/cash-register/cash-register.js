d3caf0d11390ee589c54ac72a8f617a907749235
Francisco Sueza <fcosueza@disroot.org>
2020-11-12 13:15:13 +0100

    Added enoughChange function and coinValues array

--- javascript-algorithms/cash-register/cash-register.js
+++ javascript-algorithms/cash-register/cash-register.js
@@ -1,7 +1,25 @@
-function checkCashRegister(price, cash, cid) {
-  var change;
+const coinValues = [
+  ["PENNY", 0.01],
+  ["NICKEL", 0.05],
+  ["DIME", 0.1],
+  ["QUARTER", 0.25],
+  ["DOLLAR", 1],
+  ["FIVE", 5],
+  ["TEN", 10],
+  ["TWENTY", 20],
+  ["HUNDRED", 100]
+];
 
-  return {};
-}
+const checkCashRegister = (price, cash, cid) => {
+  let change = {
+    status: "CLOSED",
+    change: []
+  };
+
+  return change;
+};
+
+const enoughChange = (change, cid) =>
+  cid.reduce((acc, curr) => acc + curr[1], 0) <= change;
 
 export default checkCashRegister;
