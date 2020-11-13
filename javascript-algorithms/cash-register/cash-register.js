5339a0477fcfdec214477ddabd9e2ee88b820b5d
Francisco Sueza <fcosueza@disroot.org>
2020-11-13 14:04:13 +0100

    Added  parseCID funtion

--- javascript-algorithms/cash-register/cash-register.js
+++ javascript-algorithms/cash-register/cash-register.js
@@ -1,15 +1,36 @@
-const denomination = [
-  { name: "PENNY", value: 0.01 },
-  { name: "NICKEL", value: 0.05 },
-  { name: "DIME", value: 0.1 },
-  { name: "QUARTER", value: 0.25 },
-  { name: "DOLLAR", value: 1.0 },
-  { name: "FIVE", value: 5.0 },
-  { name: "TEN", value: 10.0 },
+const denom = [
+  { name: "ONE HUNDRED", value: 100.0 },
   { name: "TWENTY", value: 20.0 },
-  { name: "HUNDRED", value: 100.0 }
+  { name: "TEN", value: 10.0 },
+  { name: "FIVE", value: 5.0 },
+  { name: "ONE", value: 1.0 },
+  { name: "QUARTER", value: 0.25 },
+  { name: "DIME", value: 0.1 },
+  { name: "NICKEL", value: 0.05 },
+  { name: "DIME", value: 0.01 }
 ];
 
-const checkCashRegister = (price, cash, cid) => null;
+// Transform cid array in a drawer object
+const parseCID = cid => {
+  cid.reduce(
+    (acc, curr) => {
+      const [name, amount] = curr;
+
+      acc.total += amount;
+      acc[name] = amount;
+
+      return acc;
+    },
+    { total: 0 }
+  );
+};
+
+// Process change devolution
+const checkCashRegister = (price, cash, cid) => {
+  let output = { status: "", change: [] };
+  let change = cash - price;
+
+  let drawer = parseCID(cid);
+};
 
 export default checkCashRegister;
