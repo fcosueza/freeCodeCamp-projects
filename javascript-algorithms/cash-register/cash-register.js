1ca88590ce771b95d09a65da02191d43a9b6983b
Francisco Sueza <fcosueza@disroot.org>
2020-11-13 14:31:25 +0100

    Added temp solution

--- javascript-algorithms/cash-register/cash-register.js
+++ javascript-algorithms/cash-register/cash-register.js
@@ -1,4 +1,4 @@
-const denom = [
+const DENOM = [
   { name: "ONE HUNDRED", value: 100.0 },
   { name: "TWENTY", value: 20.0 },
   { name: "TEN", value: 10.0 },
@@ -7,11 +7,11 @@ const denom = [
   { name: "QUARTER", value: 0.25 },
   { name: "DIME", value: 0.1 },
   { name: "NICKEL", value: 0.05 },
-  { name: "DIME", value: 0.01 }
+  { name: "PENNY", value: 0.01 }
 ];
 
 // Transform cid array in a drawer object
-const parseCID = cid => {
+const parseCID = cid =>
   cid.reduce(
     (acc, curr) => {
       const [name, amount] = curr;
@@ -23,14 +23,46 @@ const parseCID = cid => {
     },
     { total: 0 }
   );
-};
 
 // Process change devolution
 const checkCashRegister = (price, cash, cid) => {
-  let output = { status: "", change: [] };
+  const drawer = parseCID(cid);
+  const output = { status: "", change: [] };
   let change = cash - price;
 
-  let drawer = parseCID(cid);
+  // Exact change in drawer
+  if (drawer.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+
+    return output;
+  }
+
+  const changeArray = DENOM.reduce((acc, curr) => {
+    let amount = 0;
+
+    while (change >= curr.value && drawer[curr.name] > 0) {
+      change -= curr.value;
+      drawer[curr.name] -= curr.value;
+      amount += curr.value;
+      change = Math.round(change * 100) / 100;
+    }
+
+    if (amount !== 0) {
+      acc.push([curr.name, amount]);
+    }
+
+    return acc;
+  }, []);
+
+  if (changeArray.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+  } else {
+    output.status = "OPEN";
+    output.change = changeArray;
+  }
+
+  return output;
 };
 
 export default checkCashRegister;
