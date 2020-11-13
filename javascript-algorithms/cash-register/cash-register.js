5711af29a4fb2a6be66fd97b70de751f17f3e905
Francisco Sueza <fcosueza@disroot.org>
2020-11-13 13:39:28 +0100

    Change dime value format

--- javascript-algorithms/cash-register/cash-register.js
+++ javascript-algorithms/cash-register/cash-register.js
@@ -1,3 +1,15 @@
-const checkCashRegister = () => null;
+const denomination = [
+  { name: "PENNY", value: 0.01 },
+  { name: "NICKEL", value: 0.05 },
+  { name: "DIME", value: 0.1 },
+  { name: "QUARTER", value: 0.25 },
+  { name: "DOLLAR", value: 1.0 },
+  { name: "FIVE", value: 5.0 },
+  { name: "TEN", value: 10.0 },
+  { name: "TWENTY", value: 20.0 },
+  { name: "HUNDRED", value: 100.0 }
+];
+
+const checkCashRegister = (price, cash, cid) => null;
 
 export default checkCashRegister;
